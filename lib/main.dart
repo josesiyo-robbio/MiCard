05ad978792bd2f254bd9fedc1e81dad1a5e43eb6
josesiyo-robbio <jose.robles.bo@gmail.com>
2024-11-23 21:46:36 -0600

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:micard/theme/theme.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() {
+void main() 
+{
   runApp(const MyApp());
 }
 
@@ -10,32 +12,33 @@ class MyApp extends StatelessWidget
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) 
+  {
     final materialTheme = MaterialTheme(ThemeData().textTheme);
-    return MaterialApp(
-        title: 'MiCard',
-        theme       :   materialTheme.light(),  
-        darkTheme   :   materialTheme.dark(), 
-        home: const Scaffold(
+    return MaterialApp
+    (
+        title     : 'MiCard',
+        theme     : materialTheme.light(),
+        darkTheme : materialTheme.dark(),
 
+        home      : Scaffold(
           body: SafeArea(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              CircleAvatar(
+              const CircleAvatar(
                 radius: 50.0,
                 backgroundImage: AssetImage('assets/images/profile.png'),
               ),
-              Text(
-                
+              const Text(
                 'Power',
                 style: TextStyle(
-                  fontSize: 42.0, 
+                  fontSize: 42.0,
                   fontFamily: 'Pacifico',
-                   color:  Color.fromARGB(255, 255, 127, 159),),
-                
+                  color: Color.fromARGB(255, 255, 127, 159),
+                ),
               ),
-              Text(
+              const Text(
                 'Full Stack Developer',
                 style: TextStyle(
                     fontSize: 22.0,
@@ -44,45 +47,68 @@ class MyApp extends StatelessWidget
                     letterSpacing: 2.0,
                     fontWeight: FontWeight.bold),
               ),
-
-              SizedBox
-              (
+              const SizedBox(
                 height: 20.0,
                 width: 150.0,
-                child: Divider(color: Color.fromARGB(255, 190, 174, 174),),
-
+                child: Divider(
+                  color: Color.fromARGB(255, 190, 174, 174),
+                ),
               ),
 
 
-              Card(
-                  margin:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.phone,
-                    ),
-                    title: Text(
-                      '+52 111 222 333',
-                      style:
-                          TextStyle( fontSize: 20.0),
-                    ),
-                  )),
+              Card
+              (
+                margin  : const EdgeInsets.symmetric( vertical: 10.0, horizontal: 25.0 ),
+                child   : ListTile
+                (
+                  leading : const Icon(Icons.phone),
+                  title   : const Text('+52 111 222 333', style: TextStyle(fontSize: 20.0)),
+                  onTap: () async 
+                  {
+                    final Uri phoneLaunchUri = Uri(
+                      scheme  : 'tel',
+                      path    : '+52111222333', 
+                    );
+                    if (await canLaunchUrl(phoneLaunchUri)) 
+                    {
+                      await launchUrl(phoneLaunchUri);
+                    } 
+                    else 
+                    {
+                      throw 'Could not open dialer';
+                    }
+                  },
+                ),
+              ),
 
 
-              Card(
+              Card
+              (
+                margin  : const EdgeInsets.symmetric( vertical: 10.0, horizontal: 25.0),
+                child : 
+                ListTile
+                (
+                  leading : const Icon(Icons.mail),
+                  title   : const Text('power@power.com', style   : TextStyle(fontSize: 20.0)),
+                  onTap   : () async 
+                  {
+                    final Uri emailLaunchUri = Uri(
+                      scheme  : 'mailto',
+                      path    : 'power@power.com',
+                    );
+                    if (await canLaunchUrl(emailLaunchUri)) 
+                    {
+                      await launchUrl(emailLaunchUri);
+                    } 
+                    else 
+                    {
+                      throw 'Could not open mail client';
+                    }
+                  },
+                ),
+              ),
 
-                  margin  :
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child : ListTile(
-                    leading : Icon(
-                      Icons.mail,
-                    ),
-                    title : Text(
-                      'power@power.com',
-                      style:
-                          TextStyle( fontSize: 20.0),
-                    ),
-                  )),
+
             ],
           )),
         ));
